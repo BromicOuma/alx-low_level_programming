11086b7e5b95cd44873c778154b3964b3d77057c
Bromic <oumabromic7@gmail.com>
2022-12-08 05:20:43 -0800

    Puts in betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/**
+ * main - The beginning of the program where comilation begans
+ *
+ * Return: Always terminates with a success.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
