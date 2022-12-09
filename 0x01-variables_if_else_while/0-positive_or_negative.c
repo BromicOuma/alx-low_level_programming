711546d91445d0ea69dca8909a6211539acc7191
Bromic <oumabromic7@gmail.com>
2022-12-09 06:04:00 -0800

    Function prints random number and prompt user

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - the main function,program execution begins here
+ *
+ * The function generates random number
+ *
+ * Return: returns success for the correct execution
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+	printf("%d number is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d number is negative\n", n);
+	}
+	else
+	{
+	printf("%d number is 0\n", n);
+	}
+
+	return (0);
+}
