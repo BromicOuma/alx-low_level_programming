1a450181642a44950547992856baa74562299b8f
Bromic <oumabromic7@gmail.com>
2022-12-10 04:34:43 -0800

    prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - The main program execution starting point
+ *
+ * Program prints lower case alphabets in reverse order i.e z-a
+ *
+ * Return: returns success on succesful program execution
+ *
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a' ; ch--)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
