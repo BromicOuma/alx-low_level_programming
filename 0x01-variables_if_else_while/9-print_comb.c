a5f32dab0ac40942f4bd9d6239114300d5e73e87
Bromic <oumabromic7@gmail.com>
2022-12-10 08:06:07 -0800

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - main function where every program execution begins
+ *
+ * The function prints real number from 0 to 9
+ *
+ * Return: returns success and exits
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+
+	if (i != 9)
+	{
+	putchar(',');
+	putchar('\t');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
