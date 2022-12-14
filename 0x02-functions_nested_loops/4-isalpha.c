ba814e17650cef3bb5df3a38fef32a31bcc657c6
Bromic <oumabromic7@gmail.com>
2022-12-14 05:14:59 -0800

    function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a character is lowercase or uppercase
+ * @c: The character to be checked
+ *
+ * Return: return 1 if character is lowercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if ((c >= 98 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
