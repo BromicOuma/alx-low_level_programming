0b7317caf91b7b3b4978c33c5cf1ff9dcfe232cf
Bromic <oumabromic7@gmail.com>
2022-12-14 06:55:32 -0800

    Absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _abs - function to compute absolute value
+ *@int: takes an integer variable
+ *Return: returns the absolute value of a number
+ */
+int _abs(int)
+{
+	int n;
+
+	n = abs(n);
+	return (n);
+
+}
