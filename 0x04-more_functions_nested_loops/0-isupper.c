8716274f3d0b3ecc30626167a4fa75c5c612a982
Bromic <oumabromic7@gmail.com>
2022-12-15 08:08:18 -0800

    Alphabet Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * _isupper - check if a letter is uppercase
+ * @c: the character
+ *
+ * Return: the function returns 1 when character is 
+ */
+
+int _isupper(int c)
+{
+	if (c => 65 && c <= 98)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
