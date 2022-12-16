06fc4831d48074403df164dfb38e154a31fa429e
Bromic <oumabromic7@gmail.com>
2022-12-16 02:32:53 -0800

    Print Most number excluding 2 and 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+void print_most_numbers(void);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,4 +8,3 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-#endif /* MAIN_H */
