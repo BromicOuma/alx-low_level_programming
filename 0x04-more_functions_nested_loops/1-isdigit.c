70be3d253b3d9b988acf51f82e4b2f66e00daac1
Bromic <oumabromic7@gmail.com>
2022-12-15 21:28:39 -0800

    Is Digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#incude "main.h"
+
+/**
+ * _isdigit - input digit i between 0 to 9
+ *
+ * @c: input value
+ * Return: Return 1 if c is digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+	return (1);
+	}
+	return (0);
+}
