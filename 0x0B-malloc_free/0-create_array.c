d457d9b9e75731ebebd5df4bc75db79291df37da
bromic <oumabromic7@gmail.com>
2023-01-10 05:13:03 -0800

    Fixed memory deallocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	{
 	return (NULL);
-	free(array);
+
 	}
 	array = malloc(sizeof(char) * size);
 
@@ -26,7 +26,7 @@ char *create_array(unsigned int size, char c)
 	*(array + i) = c;
 	}
 	return (array);
-	free(array);
+
 }
 
 
