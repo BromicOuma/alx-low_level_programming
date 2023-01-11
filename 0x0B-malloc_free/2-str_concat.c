adb2533a5aac103c5f50e9c2ba9d255b075c7f4c
bromic <oumabromic7@gmail.com>
2023-01-11 01:40:02 -0800

    Concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,43 +13,39 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, l;
 	char *cat;
 
-	if (s1 == NULL || s2 == NULL)
-	return ('\0');
-
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	i = 0;
 	while (s1[i] != '\0')
-	{
-	i++;
-	}
-
+		{
+		i++;
+		}
 	j = 0;
 	while (s2[j] != '\0')
-	{
-	j++;
-	}
-
-	k = i + j;
+		{
+		j++;
+		}
+	k = i + j + 1;
 	cat  = malloc(sizeof(char) * k);
 	if (cat == NULL)
-	return (NULL);
-
+		return (NULL);
 	j = 0;
 	l = 0;
 	while (l < k)
-	{
-	if (l <= i)
-	{
-	cat[l] = s1[l];
-	}
-
-	if (l >= i)
-	{
-	cat[l] = s2[j];
-	j++;
-	}
-	l++;
-	}
-	cat[l] = '\0';
+		{
+		if (l <= i)
+			{
+			cat[l] = s1[l];
+			}
+		if (l >= i)
+			{
+			cat[l] = s2[j];
+			j++;
+			}
+		l++;
+		}
+		cat[l] = '\0';
 	return (cat);
-
 }
