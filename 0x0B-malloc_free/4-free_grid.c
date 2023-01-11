ed2e64f335fadb2656e9a8646199957aa929818a
bromic <oumabromic7@gmail.com>
2023-01-11 07:56:45 -0800

    Free memory allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+#include <stdlib.h>
+
+
+
+/**
+
+  * free_grid - function to free memory allocation...
+
+  * @grid: pointer to pointer of grid function
+
+  * @height: the height
+
+  *
+
+  * Return: freed grid
+
+  */
+
+void free_grid(int **grid, int height)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < height; i++)
+
+	{
+
+		free(grid[i]);
+
+	}
+
+
+
+	free(grid);
+
+}
