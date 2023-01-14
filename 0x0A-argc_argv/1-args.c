29dbd12994dadd826b20cf05d6b471122e7ee0b0
bromic <oumabromic7@gmail.com>
2023-01-14 08:47:51 -0800

    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

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function taking commandline parameters
+ *
+ * @argc: count of arguments
+ * @argv: pointer to array of characters
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	(void) **argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
