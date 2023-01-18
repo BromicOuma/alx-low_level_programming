4a1d8587b8a39faf0e1781e7aa1b68d6f5b0658b
bromic <oumabromic7@gmail.com>
2023-01-18 05:12:35 -0800

    Type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_
+#define DOG_
+
+/**
+ * struct dog - type dog with this members
+ * @name: name of dog
+ * @age: age of dog
+ * @type: dog breed
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
+
+
+
