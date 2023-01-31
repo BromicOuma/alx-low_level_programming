69f6d2c990d3d6789acee334c819c485643da8ea
bromic <oumabromic7@gmail.com>
2023-01-31 02:37:02 -0800

    Written a function that counts the number of elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print count of lists
+ * @h: pointer to the structure
+ * Return:Number of elements of a linkedlist
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned counter = 0;
+
+	while (h != NULL)
+	{
+	h = h -> next;
+	counter++;
+	}
+	return (counter);
+}
