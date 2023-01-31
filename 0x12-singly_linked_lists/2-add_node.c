67480dc575e9474009b744ea92cafe4df35c243f
bromic <oumabromic7@gmail.com>
2023-01-31 08:55:53 -0800

    Added betty documentation alignment

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,67 +1,38 @@
 #include "lists.h"
-
 #include <stdlib.h>
 
 /**
-
   * _strlen - gets length of the string
-
   * @s: string
-
   * Return: length of the string
-
   */
-
 int _strlen(const char *s)
-
 {
-
 	int i;
 
-
-
 	for (i = 0; s[i]; i++)
-
 		;
-
 	return (i);
-
 }
 
 /**
-
   * add_node - add new nodes to the list
-
   * @head: current place in the list
-
   * @str: string to add to the head
-
   * Return: pointer to current position in list
-
   */
-
 list_t *add_node(list_t **head, const char *str)
-
 {
-
 	int i, len;
-
 	char *content;
-
 	list_t *new;
-
-
 
 	if (str == NULL || head == NULL)
 
 		return (NULL);
-
 	len = _strlen(str);
-
 	new = *head;
-
 	content = malloc((len + 1) * sizeof(char));
-
 	if (content == NULL)
 
 		return (NULL);
