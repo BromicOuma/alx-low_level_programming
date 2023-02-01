5f3efb5c4d00de9cc67560ea90532d6dbb957545
bromic <oumabromic7@gmail.com>
2023-01-31 21:17:05 -0800

    Executes before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
 /**
-
   * b - prints from init by the loader before main
-
   */
-
 void b(void) __attribute__ ((constructor));
-
 void b(void)
-
 {
 
 	printf("You're beat! and yet, you must allow,\n");
