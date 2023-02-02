a8b4166c340e38785203c7a94c59b4c48f3f3cf3
bromic <oumabromic7@gmail.com>
2023-02-01 22:16:18 -0800

    ::

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+  * add_nodeint - functions that adds a node
+  * @head: pointer to head pointer
+  * @n: data to be added
+  * Return: return the new memory allocated for the list
+  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
