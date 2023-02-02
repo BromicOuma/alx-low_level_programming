841b6c82e52118dd2dc8454c6ab72b32ce8a7916
bromic <oumabromic7@gmail.com>
2023-02-02 02:54:52 -0800

    ::

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 
 /**
-
   * add_nodeint_end - add new nodes to the end of the list
-
   * @head: current place in the list
-
   * @n: int to add to the list
-
   * Return: pointer to current position in list
-
   */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
